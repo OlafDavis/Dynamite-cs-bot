800fe484e97cfe817f32b81aedd2c33320b0c458
Olaf Davis <olaf.davis@softwire.com>
2021-10-01 00:04:42 +0100

    chooses random move, stops W after 100 enemy D

--- ExampleBot/ExampleBot.cs
+++ ExampleBot/ExampleBot.cs
@@ -1,5 +1,7 @@
 ﻿using BotInterface.Bot;
 using BotInterface.Game;
+using System;
+using System.Collections.Generic;
 
 namespace ExampleBot
 {
@@ -7,7 +9,23 @@ namespace ExampleBot
     {
         public Move MakeMove(Gamestate gamestate)
         {
-            return Move.P;
+            int myDynamite = 100;
+            int opponentDynamite = 100;
+            var rounds = gamestate.GetRounds();
+            for (int i = 0; i < rounds.Length; i++)
+            {
+                if (rounds[i].GetP1() == Move.D) myDynamite--;
+                if (rounds[i].GetP2() == Move.D) opponentDynamite--;
+            }
+
+            var options = new List<Move> { Move.R, Move.S, Move.P };
+
+            if (myDynamite > 0) options.Add(Move.D);
+            if (opponentDynamite > 0 && opponentDynamite < 100) options.Add(Move.W);
+
+            var random = new Random();
+            int index = random.Next(options.Count);
+            return options[index];
         }
     }
 }
